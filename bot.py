2f25fe14532d5592b9988903140ce0d78ae63d22
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-08-21 20:22:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,8 @@ from utils import download_and_rename_subtitle
 # -------------------------
 # Env & logging
 # -------------------------
+from dotenv import load_dotenv
+load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 if not TELEGRAM_TOKEN:
     raise RuntimeError("TELEGRAM_TOKEN is not set")
